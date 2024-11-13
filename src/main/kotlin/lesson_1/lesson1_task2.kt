3116d8ed1dbffd183b41c9901cddb20c26a5b854
Asplet <aspletrise@gmail.com>
2024-11-14 02:35:08 +0300

    KS-1-2 delete U-type, change var to val

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,13 +3,13 @@ package org.example.lesson_1
 fun main(){
     val thanksForPurchaseText: String = "Благодарим за покупку! Электронный чек будет выслан на указанный Вами адрес электронной почты."
 
-    var orderCounter: UInt = 75u
-    var totalNumberOfEmployees: UInt = 2000u
+    val orderCounter: Int = 75
+    var totalNumberOfEmployees: Int = 2000
 
     println("Общее количество заказов: " + orderCounter)
 //    println("Общее количество работников магазина: " + totalNumberOfEmployees)
 
-    totalNumberOfEmployees -= 1u
+    totalNumberOfEmployees -= 1
 
     println("Общее количество работников магазина: " + totalNumberOfEmployees)
     println(thanksForPurchaseText)
