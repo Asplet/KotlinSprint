2e3fbc3fca0152d9f2924d6a0520d72c53999ecb
Asplet <aspletrise@gmail.com>
2024-11-14 01:47:59 +0300

    KS-1-3 outputting Gagarin's fly time

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main(){
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println("0" + hour)
+    println("0" + minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    println(":" + minute)
+
+}
