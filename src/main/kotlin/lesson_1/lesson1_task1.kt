74b92e6d053213ac8edbf31f0e3cfc7cac5deb1e
Asplet <58989832+Asplet@users.noreply.github.com>
2024-11-14 23:42:16 +0300

    KS-1-1 creating a file with two variables (#1)
    * KS-1-1 creating a file with two variables
    
    * KS-1-1 delete U-type, change var to val

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,6 @@
+package org.example.lesson_1
+
+fun main(){
+    val orderCounter: Int = 75
+    val thanksForPurchaseText: String = "Благодарим за покупку! Электронный чек будет выслан на указанный Вами адрес электронной почты."
+}
