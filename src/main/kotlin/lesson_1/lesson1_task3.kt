8ed96ed5d0dc616d8391a1a791a3666bff43b82d
Asplet <aspletrise@gmail.com>
2024-11-14 12:01:41 +0300

    KS-1-3 refactor logic

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,18 +1,20 @@
 package org.example.lesson_1
 
 fun main() {
-    val year = "1961"
-    var hour = "09"
-    var minute = "07"
+    val year = 1961
+    var hour = 9
+    var minute = 7
 
+    val takeoffHour = String.format("%02d", hour)
+    val takeoffMinute = String.format("%02d", minute)
     println(year)
-    println(hour)
-    println(minute)
+    println(takeoffHour)
+    println(takeoffMinute)
 
-    hour = "10"
-    minute = "55"
+    hour = 10
+    minute = 55
 
-    print(hour)
-    println(":" + minute)
+    val landingTime = String.format("%02d:%02d", hour, minute)
+    println(landingTime)
 
 }
