cd0eabc1c2c230f2787fc4b27237894a86234aa6
Asplet <aspletrise@gmail.com>
2024-11-16 00:00:34 +0300

    KS-2-4 refactor percent format

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
-const val EXTRACTION_BUFF = 0.2f
-
 fun main() {
 
     val crystalOre = 7
     val ironOre = 11
 
-    val bonusCrystalOre = String.format("%.0f", (crystalOre * EXTRACTION_BUFF))
-    val bonusIronOre = String.format("%.0f", (ironOre * EXTRACTION_BUFF))
+    val extractionBuff = 20
+    val bonus = extractionBuff / 100f
+
+    val bonusCrystalOre = String.format("%.0f", (crystalOre * bonus))
+    val bonusIronOre = String.format("%.0f", (ironOre * bonus))
 
     println("Бонус кристаллической руды: $bonusCrystalOre")
     println("Бонус железной руды: $bonusIronOre")
