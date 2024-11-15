446cd6a41576e140987da92e5eee3351bd673be4
Asplet <aspletrise@gmail.com>
2024-11-15 23:54:32 +0300

    KS-2-1 refactor with listOf()

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,13 +2,10 @@ package org.example.lesson_2
 
 fun main() {
 
-    val numberOfStudents: Byte = 4
-    val gradeOfStudent_1 = 3f
-    val gradeOfStudent_2 = 4f
-    val gradeOfStudent_3 = 3f
-    val gradeOfStudent_4 = 5f
-
-    val arithmeticMeanGrade = (gradeOfStudent_1 + gradeOfStudent_2 + gradeOfStudent_3 + gradeOfStudent_4) / numberOfStudents
+    val allGrades = listOf(3, 4, 3, 5)
+    val arithmeticMeanGrade = allGrades.sum().toFloat() / allGrades.size
+    //насколько понимаю это^ ещё можно заменить и этим: val arithmeticMeanGrade = allGrades.average()
 
     println(arithmeticMeanGrade)
+
 }
