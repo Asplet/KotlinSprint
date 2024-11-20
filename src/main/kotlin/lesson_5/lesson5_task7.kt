663c6b913112690abf7257c4210bb7aa6c0395ea
Asplet <aspletrise@gmail.com>
2024-11-21 01:03:58 +0300

    KS-5-6 calculator of fuel

---
+++ src/main/kotlin/lesson_5/lesson5_task7.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println("Введите расстояние поездки, км: ")
+    val userDistance = readln().toFloat()
+    println("Введите расход топлива на 100 км, л: ")
+    val userFuelConsumption = readln().toFloat()
+    println("Введите текущую цену на топливо, руб.: ")
+    val userFuelPrice = readln().toFloat()
+
+    val fuelForTrip = (userDistance * userFuelConsumption) / 100
+
+    val totalFuelPrice = fuelForTrip * userFuelPrice
+
+    println("Для поездки необходимо топлива: ${String.format("%.2f", fuelForTrip)} л.\n" +
+            "Общая стоимость поездки: ${String.format("%.2f", totalFuelPrice)} руб.")
+}
